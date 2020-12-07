58290bf7777af6af88238026b03a842a1f30a715
utkarshA-optimus <utkarsh.awasthi@optimusinfo.com>
2020-12-07 22:35:42 +0530

    fixes in render on signing in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -17,15 +17,15 @@ const Header = ({ currentUser }) => (
             <Link className='option' to='/shop'>CONTACT</Link>
             {
                 currentUser ?
-                    <div className='option' onClick={() => auth.signOut()}> SIGN OUT</div> :
-                    <Link className='option' to='/signin'>SIGN IN</Link>
+                    (<div className='option' onClick={() => auth.signOut()}> SIGN OUT</div>) :
+                    (<Link className='option' to='/signin'>SIGN IN</Link>)
             }
         </div>
     </div>
 )
 
 const mapStateToProps = state => ({
-    currentState: state.user.currentUser
+    currentUser: state.user.currentUser
 })
 
 export default connect(mapStateToProps)(Header);
