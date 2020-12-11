b5c7772352e4b038b10c68a75a3dc5d700d44ea6
utkarshA-optimus <utkarsh.awasthi@optimusinfo.com>
2020-12-12 00:26:56 +0530

    added state persistence

--- src/reducer/root-reducer.js
+++ src/reducer/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import CartReducer from './cart/cart.reducer';
 
 import userReducer from './user/user-redux';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: CartReducer
-})
+});
+
+export default persistReducer(persistConfig, rootReducer);
